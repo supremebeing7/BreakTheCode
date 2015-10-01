3d3deedef67f51cc7bfec535bcbf5dea034436f4
Mark Lehman <markopolo@gmail.com>
2015-10-01 14:25:30 -0700

    add intents

--- app/src/main/java/com/markjlehman/breakthecode/MainActivity.java
+++ app/src/main/java/com/markjlehman/breakthecode/MainActivity.java
@@ -1,17 +1,50 @@
 package com.markjlehman.breakthecode;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+
+    private Button mEnrollButton;
+    private Button mSponsorsButton;
+    private Button mPartnersButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEnrollButton = (Button) findViewById(R.id.enrollButton);
+        mSponsorsButton = (Button) findViewById(R.id.sponsorsButton);
+        mPartnersButton = (Button) findViewById(R.id.partnersButton);
+
+        mEnrollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent enrollIntent = new Intent(MainActivity.this, EnrollActivity.class);
+                startActivity(enrollIntent);
+            }
+        });
+        mSponsorsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sponsorsIntent = new Intent(MainActivity.this, SponsorsActivity.class);
+                startActivity(sponsorsIntent);
+            }
+        });
+        mPartnersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent partnersIntent = new Intent(MainActivity.this, PartnersActivity.class);
+                startActivity(partnersIntent);
+            }
+        });
     }
 
 
@@ -28,9 +61,6 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 }
